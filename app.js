f5917ae45e00439f215a85441f6a93e5c8d7e7ae
sakshi192000 <sakshisawants4100@gmail.com>
2021-08-04 20:37:58 +0530

    refactor

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 // Navigation Section
 const navToggle = document.querySelector(".navbar_toggle");
 const links = document.querySelector(".main_nav");
+const navlinks = document.querySelectorAll(".nav_links");
 
 navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
+    navlinks.classList.toggle("show_nav");
 })
 
